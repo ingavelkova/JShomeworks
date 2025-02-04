e4ce9fdc3a296d69f16e446dd7197a9bf6c24554
Vielkova Inga <ingavelkova@gmail.com>
2025-02-04 20:26:21 +0200

    added negative power

--- homework2.js
+++ homework2.js
@@ -2,13 +2,23 @@
 
 function pow(x,y) {
     let result = 1;
-
-    while (y) {
-        result = x * result;
-        y--;
+    let temp=1;
+   
+    if (y>=0) {    
+    
+        while (y){
+            result = x * result;
+            y--;
+        };
+    } else {    
+        
+        while (y<0) {
+            temp = x*temp;
+            result = 1/temp;
+            y++;
+        };
     };
-
-    return result;
+    return result; 
 };
 
 console.log('Result of exponentiation: ', pow(2,3));
